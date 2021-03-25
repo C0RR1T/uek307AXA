6deffed25e550e64588cc27b230bd91e21c3527e
yagan93 <51398865+yagan93@users.noreply.github.com>
2021-03-25 15:28:48 +0100

    Create user.java

---
+++ user.java
@@ -0,0 +1,25 @@
+
+package ch.noser.uek223ex8.domain.user;
+
+import ch.noser.uek223ex8.domain.post.Post;
+import ch.noser.uek223ex8.domain.role.Role;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
